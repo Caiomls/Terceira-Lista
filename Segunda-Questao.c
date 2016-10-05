ec27b79fef1194efa36b74f7e3bc0f7d5b828738
Caiomls <Caiomls12@gmail.com>
2016-10-05 20:00:27 -0300

    Update Segunda-Questao.c

--- Segunda-Questao.c
+++ Segunda-Questao.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+//Não mais cagado
 
-struct coordenada{
+int X, Y;
+struct coordenada {
   int x;
-  int y;};
+  int y;
+};
 
 int main(){
-struct coordenada c;
-  scanf("%d", &c.x);
-  scanf("%d", &c.y);
+  struct coordenada c;
+    scanf ("%d", &c.x);
+    scanf ("%d", &c.y);
 
-printf("Distancia da coordenada inserida ate a origem eh (%d,%d)",c.x,c.y);
+  struct coordenada d;
+    scanf ("%d", &d.x);
+    scanf ("%d", &d.y);
+  if (c.x>d.x) {
+    X = (c.x-d.x);
+  }
+  else{
+    X = (d.x-c.x);
+  }
+  if (c.y>d.y) {
+    Y = (c.y-d.y);
+  }
+  else  {
+    Y = (d.y-c.y);
+  }
+  printf ("Distancia eh de (%d,%d)",X,Y);
+  system("pause");
 
-return 0;}
+return (0);
+}
