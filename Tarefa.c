8fd51c664b88a57eaa234120a94c356a668b53e3
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2016-10-05 19:37:24 -0300

    Update Tarefa.c

--- Tarefa.c
+++ Tarefa.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 struct cadrasto{
-    char nome;
+    char nome[50];
     int idade;
+    char rua[50];
+    int numero;
     
 };
 int main()
 {
-    printf("Hello world!\n");
+    struct cadastro c;
+    gets("Digite o seu nome: \n", c.nome);
+    scanf("Digite sua idade: %d", &c.idade);
+    gets("Digite o sua rua: \n", c.rua);
+    scanf("Digite o numero da residencia: %d", &c.numero);
+    system("pause");
     return 0;
 }
