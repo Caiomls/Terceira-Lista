e4c899019761379f3392696d1a2a0743466b2a21
Caiomls <Caiomls12@gmail.com>
2016-10-05 19:42:19 -0300

    Update terceira.c

--- terceira.c
+++ terceira.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Completamente cagado
+
+int distancia, distancia2;
+struct X {
+  int x;
+  int y;
+}
+
+struct Y {
+  int z;
+  int w;
+}
+
+int main(){
+ 
+
+return (0);
+}
 
