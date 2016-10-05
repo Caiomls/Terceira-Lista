48767c9773880135bb89bb91b5a6e61273ca3edd
Caiomls <Caiomls12@gmail.com>
2016-10-05 19:50:44 -0300

    Update terceira.c

--- terceira.c
+++ terceira.c
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 //Completamente cagado
 
-int distancia, distancia2;
-struct X {
+int distanciax, distanciay;
+struct coordenada {
   int x;
   int y;
 };
 
-struct Y {
-  int z;
-  int w;
-};
-
 int main(){
- 
+  struct coordenada c;
+    scanf("%d", &c.x);
+    scanf("%d", &c.y);
+  
+  struct coordenada d;
+    scanf("%d", &d.x);
+    scanf("%d", &d.y);
+  if c.x>d.x {
+    distanciax = (c.x-d.x)
+  }
+  else{
+    distanciax = (d.x-c.x)
+  }
+  if c.y>d.y {
+    distanciay = (c.y-d.y)
+  }
+  else  {
+    disntaciay = (d.y-c.y)
+  }
+  printf ("Distancia eh de (%d,%d)",disanciax,disntanciay)
 
 return (0);
 }
