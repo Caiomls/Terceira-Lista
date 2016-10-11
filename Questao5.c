9ac1f02f657bd235cddccdd5db95bf5fdbb39a1e
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2016-10-11 15:38:18 -0300

    done

--- Questao5.c
+++ Questao5.c
@@ -4,12 +4,43 @@
 struct aluno{
     char nome[50];
     int matric;
-    int nota1;
-    int nota2;
-    int nota3;
+    float nota1;
+    float nota2;
+    float nota3;
+    int i;
 
 };
 int main(){
+    i==0;
+    do{
+    printf("Nome do aluno..:: ");
+    fflush(stdin);
+
+    fgets(aluno.nome, 40, stdin);
+
+    printf("Informe a matricula ..: ");
+    scanf("%d", &aluno.matric);
+
+    printf("Informe a 1a. nota ..: ");
+    scanf("%f", &aluno.nota1);
+
+    printf("Informe a 2a. nota ..: ");
+    scanf("%f", &aluno.nota2);
+
+    printf("Informe a 3a. nota ..: ");
+    scanf("%f", &aluno.nota3);
+
+    printf("\n\n --------- Lendo os dados da struct ---------\n\n");
+    printf("Nome ...........: %s", aluno.nome);
+    printf("Disciplina .....: %d", aluno.matric);
+    printf("Nota da Prova 1 ...: %.2f\n" , aluno.nota1);
+    printf("Nota da Prova 2 ...: %.2f\n" , aluno.nota2);
+    printf("Nota da Prova 3 ...: %.2f\n" , aluno.nota3); 
+    getch();
+    i+=1
+    return(0);
+    }while (i<5);
+}
 
 
 
